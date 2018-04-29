6b9d2f9ed4f696e7bc54ad0c8ab6debfad8fae95
saisf <sleung82@mail.ccsf.edu>
2018-04-28 21:32:48 -0700

    Add customized collection view

--- MeMe/Controller/MemedCollectionViewController.swift
+++ MeMe/Controller/MemedCollectionViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class MemedCollectionViewController: UIViewController {
+class MemedCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var memedCollectionView: UICollectionView!
+    
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        memedCollectionView.dataSource = self
+        memedCollectionView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +31,12 @@ class MemedCollectionViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath)
+    }
 
 }
